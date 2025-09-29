953016b9e6f042ad30ecd209a155eee5f0ed73db
Christopher <chrisgomand.9@gmail.com>
2025-09-29 01:42:01 -0600

    Hola mundo. Hola, JS.

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,20 @@
+// Esto es un comentario simple
+
+/*
+Este es un
+comentario
+de varias lineas
+*/
+
+console.log("¡Hello, World!")
+console.log('Mi nombre es Christopher')
+console.log(`Tengo 18 años`)
+
+console.log(5)
+console.log("5")
+console.log(5+10)
+console.log(10-3)
+console.log(10*3)
+console.log(5/2)
+console.log(9%10)
+console.log(2**2)
